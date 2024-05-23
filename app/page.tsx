b64bb68ea4be9ac28b2f07e168d7d749985cbcbe
Sandesh thapa <sandesh.thapa@braintip.ai>
2024-05-23 22:42:43 +0545

    hlo man

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <h3 className="capitalize"> hello k xa timor khabar</h3>
+      <h3 className="capitalize"> hello k xa timor khabar dfdsfsdf dsf</h3>
     </div>
   );
 }
